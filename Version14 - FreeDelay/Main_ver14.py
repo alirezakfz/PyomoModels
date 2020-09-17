eb4cf8b4bc345cd293f602bbe756d0ae8bf0c6a0
Alireza Khaksari <alireza_kfz@yahoo.com>
2020-09-18 00:28:53 +0300

    Free Delay Model

---
+++ Version14 - FreeDelay/Main_ver14.py
@@ -0,0 +1,249 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 15 16:45:29 2020
+
+@author: alire
+"""
+
+import time
+import numpy as np
+from pyomo.environ import *
+from pyomo.opt import SolverFactory
+
+from ConcreteModel_ver13_spyder_04 import createModel
+from CreateData import dataFile
+from chart import gant_chart
+from save_data import save_scenario
+from save_data import save_model
+from save_data import csv_file
+
+"""
+Defining the scenario parameters
+"""
+
+slot=1   #Add more time slot for more accurate result
+
+number_of_EVs=20
+
+number_of_Chargers=0  #took it from dataFile output later
+
+number_of_scenarios=1
+           
+number_of_timeslot=24*slot
+
+Charger_Type=[4, 8, 19, 50]     #type of chargers to install
+
+charger_cost=[1000,1500,2200, 50000]  #cost of installation
+
+
+
+#declaring variables
+arrival=list()
+depart=list()
+distance=list()
+demand=list()
+charge_power=np.empty([10,4])
+installed_chargers=list()
+installed_cost=list()
+TFC=np.empty([10,4])
+EV_samples=list()
+list_data=list()
+
+
+"""
+Stroing the result and scenarios in list
+"""
+
+#save model data
+model_data=[]
+
+#save EVs Info
+list_row=[]
+list_data=[]
+
+#save scenario data
+list_header=["Model","Scenario","Number_of_EVs","EV_No","Arrival","Depart","EV_Type","BatteryCapacity","Distance","Demand","Alloc_charger","Delay"]
+#list_row.append(list_header)
+
+#row to be add as aheader of model_data: store Scenarios information
+row=["Model","scenario","obj_value","number_of_EVs","Demand"]
+
+for ch in Charger_Type:
+    row.append('AvailabeType_'+str(ch))
+    row.append('InstalledType_'+str(ch))
+
+#The first row Initialized    
+#model_data.append(row)
+
+
+"""
+Start Scenario Creation and execution
+"""
+start_time= time.time()
+
+scenario_model="FreeDelay"
+solver_name="cplex"
+
+#Call csv file creator to store results
+file_model , file_data = csv_file(list_header,row,scenario_model)
+
+
+# solver=SolverFactory(solver_name)
+
+for scenario in range(1,number_of_scenarios+1):
+    
+    arrival.clear()
+    depart.clear()
+    distance.clear()
+    demand.clear()
+    charge_power=np.empty([1,1])
+    installed_chargers.clear()
+    installed_cost.clear()
+    TFC=np.empty([1,1])
+    EV_samples.clear()
+    list_data.clear()
+    
+    #create a scenario data
+    arrival, depart, distance, demand, charge_power,installed_chargers,\
+             installed_cost,TFC, EV_samples = dataFile(number_of_EVs,
+                                                       number_of_timeslot,
+                                                       Charger_Type,
+                                                       charger_cost,
+                                                       slot)
+             # arrival,
+             # depart,
+             # distance,
+             # demand,
+             # charge_power,
+             # installed_chargers,
+             # installed_cost,
+             # TFC,
+             # EV_samples)
+    
+    
+    """
+    Calling the model creator function based on generated data
+    """
+    number_of_Chargers=len(installed_chargers)
+    #create pyomo model
+    model=createModel(number_of_EVs, number_of_Chargers, number_of_timeslot,
+                      installed_chargers, installed_cost, arrival,depart, TFC)
+    
+    """
+    solve the model
+    """    
+    solver=SolverFactory(solver_name)
+    results = solver.solve(model)
+    
+    while True:
+        if (results.solver.status == SolverStatus.ok) and (results.solver.termination_condition == TerminationCondition.optimal):
+            #if solver find an feasible solution
+            list_data=save_scenario(number_of_EVs,
+                              number_of_Chargers,
+                              arrival, 
+                              depart, 
+                              distance, 
+                              demand, 
+                              charge_power,
+                              installed_chargers, 
+                              installed_cost, 
+                              TFC,
+                              model,
+                              EV_samples,
+                              scenario,
+                              scenario_model) 
+            #check if there is error in data
+            if list_data:
+                break
+            else:
+                #create new scenario data 
+                arrival, depart, distance, demand, charge_power,installed_chargers,\
+                    installed_cost,TFC, EV_samples = dataFile(number_of_EVs,
+                                                               number_of_timeslot,
+                                                               Charger_Type,
+                                                               charger_cost,
+                                                               slot)
+                
+                number_of_Chargers=len(installed_chargers)
+                
+                model=createModel(number_of_EVs, number_of_Chargers, number_of_timeslot,
+                      installed_chargers, installed_cost, arrival,depart, TFC)
+                
+                results = solver.solve(model)
+                
+        #check for data           
+        elif (results.solver.termination_condition == TerminationCondition.infeasible or results.solver.status != SolverStatus.ok ):
+            # Do something when model in infeasible
+            print("******** Infeasible Problem ************")  
+            dt=time.time()
+            
+            #create new scenario data
+            arrival, depart, distance, demand, charge_power,installed_chargers,\
+                installed_cost,TFC, EV_samples = dataFile(number_of_EVs,
+                                                          number_of_timeslot,
+                                                          Charger_Type,
+                                                          charger_cost,
+                                                          slot)
+                                    
+            number_of_Chargers=len(installed_chargers)
+            
+            model=createModel(number_of_EVs, number_of_Chargers, number_of_timeslot,
+                  installed_chargers, installed_cost, arrival,depart, TFC)
+            
+            results = solver.solve(model)
+            
+    "Add result from the solver to store in SCV file"
+    for row in list_data:
+        list_row.append(row)
+    
+    row=[scenario_model,scenario,value(model.obj),number_of_EVs,sum(demand)]
+    
+    available_dict={i:0 for i in Charger_Type}
+    chargers_dict={i:0 for i in Charger_Type}
+    
+    for j in model.M:
+        #Count number of chargers of each type in the scenario before solving the model
+        available_dict[installed_chargers[j-1]] +=1 
+        #Count the number of installed chargers after solving model
+        if model.q[j]==1:
+            chargers_dict[installed_chargers[j-1]]+=1 
+    
+    for i in Charger_Type:
+        row.append(available_dict[i])
+        row.append(chargers_dict[i])
+        
+    model_data.append(row)
+    
+    #print infor about number of scenarios
+    if scenario % 10 ==0:
+        number_of_EVs +=5
+        check= save_model(model_data, list_data, file_model , file_data)
+        if check:
+            model_data.clear()
+            list_data.clear()
+            print("Saved data for scenario:",scenario)
+            
+            
+        
+        # print("solved scenarios:",scenario)
+    
+    if scenario % 10 ==0:
+        print("solved scenarios:",scenario)
+    
+#    gant_chart(model)
+
+
+"""
+All the scenarios executed succesfully
+Storing data and results
+"""
+print(">>>>>>>>>  time taken:{:0.3f} <<<<<<<".format(time.time()-start_time))
+
+gant_chart(model)
+# check=save_model(model_data, list_data, file_model , file_data)   
+ 
+        
+    
+    
+    
+    
---
+++ Version14 - FreeDelay/save_data.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  3 16:59:58 2020
+
+@author: Ali
+"""
+from pyomo.environ import *
+from csv import writer
+import time
+
+
+    
+
+def save_scenario(number_of_EVs,
+              number_of_Chargers,
+              arrival, 
+              depart, 
+              distance, 
+              demand, 
+              charge_power,
+              installed_chargers, 
+              installed_cost, 
+              TFC,
+              model,
+              EV_samples,
+              scenario,
+              scenario_model
+              ):
+    
+    # list_header=["senario","Number_of_EVs","EV_NO","Arrival","Depart","Type","BatteryCapacity","Distance","Demand","alloc_charger","delay"]
+    
+    EV_types={   
+            "Small":{
+                    "energy_consumption":0.3790,
+                    "capacity":16,  
+                    "max_distance":42.2163,
+                    "charge_rate":8
+                    },
+            "Sedan":{
+                    "energy_consumption":0.4288,
+                    "capacity":24,
+                    "max_distance":55.9701,
+                    "charge_rate":19
+                    },
+            "SUV":{
+                    "energy_consumption":0.5740,
+                    "capacity":54,
+                    "max_distance":94.0766,
+                    "charge_rate":50
+                    },
+            "Truck":{
+                    "energy_consumption":0.8180,
+                    "capacity":70,
+                    "max_distance":85.5745,
+                    "charge_rate":50
+                    }
+            }
+    
+    list_row=[]
+    
+#    if scenario==1:
+#        list_row.append(list_header)
+
+    alloc_charger=[]
+    is_charged=False
+    
+    for ev in model.N:
+        sum_ch=sum(value(model.x[ch,ev,t]*ch) for ch in model.M for t in model.T)
+        sum_ch=int(sum_ch)
+        if sum_ch > 0:
+            alloc_charger.append(installed_chargers[sum_ch-1])
+        else:
+            print("NAN")
+            return False
+            
+    
+    
+    # alloc_charger=[]
+    # is_charged=False
+    # for ev in model.N:
+    #     is_charged=False
+    #     for ch in model.M:
+    #         for t in model.T:
+    #             if value(model.x[ch,ev,t])==1:
+    #                 is_charged=True
+    #                 # alloc_charger.append("Type_"+str(installed_chargers[ch-1]))
+    #                 alloc_charger.append(installed_chargers[ch-1])
+    #     if not is_charged:
+    #         # alloc_charger.append("NAN")
+    #         print("NAN")
+    #         return False
+                    
+    
+    for i in range(number_of_EVs):    
+        if value(model.C[i+1])-value(model.depart[i+1]) > 0 :
+            delay=value(model.C[i+1])-value(model.depart[i+1])
+        else:
+            delay=0
+         
+        row=[scenario_model,
+             scenario,
+             number_of_EVs,
+             i+1,
+             arrival[i],
+             depart[i],
+             EV_samples[i],
+             EV_types[EV_samples[i]]["capacity"],
+             distance[i],
+             demand[i],
+             alloc_charger[i],
+             delay]
+        
+        list_row.append(row)
+     
+    return list_row   
+        
+#    if scenario==1:
+#        timestr = time.strftime("%Y%m%d-%H%M%S")
+#        file_name='EVs_Info_'+timestr+'.csv'
+#        
+#        with open(file_name, 'w', newline='') as file:
+#            csv_writer = writer(file)
+#            for rw in list_row:
+#                csv_writer.writerow(rw)
+#    else:
+#        file_name=name
+#        with open(file_name, 'a+', newline='') as file:
+#            csv_writer = writer(file)
+#            for rw in list_row:
+#                csv_writer.writerow(rw)
+    
+#    return file_name
+
+#create required files to store results
+def csv_file(list_header,row,scenario_model):
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    
+    file_model = 'Model_data_'+scenario_model+'_'+timestr+'.csv'
+    with open(file_model, 'a+', newline='') as file:
+            csv_writer = writer(file)
+            csv_writer.writerow(row)
+    
+    
+    file_data = 'EVs_Info_'+scenario_model+timestr+'.csv'
+    with open(file_data, 'a+', newline='') as file:
+            csv_writer = writer(file)
+            csv_writer.writerow(list_header)
+            
+    return file_model,file_data
+
+def save_model(model_data, list_data, file_model , file_data):
+    # timestr = time.strftime("%Y%m%d-%H%M%S")
+    # file_name='Model_data_'+timestr+'.csv'
+    # file_name='Model_data_'+scenario_model+'_'+timestr+'.csv'
+    
+    with open(file_model, 'a+', newline='') as file:
+            csv_writer = writer(file)
+            for rw in model_data:
+                csv_writer.writerow(rw)
+                
+    # file_name='EVs_Info_'+scenario_model+timestr+'.csv'
+    with open(file_data, 'a+', newline='') as file:
+            csv_writer = writer(file)
+            for rw in list_data:
+                csv_writer.writerow(rw)
+    return True
